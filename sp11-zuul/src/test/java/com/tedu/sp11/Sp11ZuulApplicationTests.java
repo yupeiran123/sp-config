5445e686e726f5a12a2596dd15e4cafb91f9097d
Administrator <Administrator@I91T1FM30VWNVX5>
2019-11-01 20:56:50 +0800

    # WARNING: head commit changed in the meantime
    5

---
+++ sp11-zuul/src/test/java/com/tedu/sp11/Sp11ZuulApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tedu.sp11;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp11ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
